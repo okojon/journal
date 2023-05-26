e8379928734404abbd6f43c063fb4044702f77fa
inoue <inoue@snark.co.jp>
2023-05-26 15:19:48 +0900

    inoue first commit

--- src/main/java/com/example/demo/JournalController.java
+++ src/main/java/com/example/demo/JournalController.java
@@ -1,5 +1,15 @@
 package com.example.demo;
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 public class JournalController {
 
+	@RequestMapping("/form")
+	public String sample(Model model) {
+		model.addAttribute("message", "日報提出");
+		return "index";
+	}
+
+	 
 }
