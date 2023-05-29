59410677ec79b26e64513302c864d78c189fd193
aaa <aaa>
2023-05-29 09:55:14 +0900

    kojima first commit

--- src/main/java/com/example/demo/JournalController.java
+++ src/main/java/com/example/demo/JournalController.java
@@ -1,5 +1,17 @@
 package com.example.demo;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class JournalController {
 
+	@RequestMapping("/form")
+	public String sample(Model model, Input input) {
+		model.addAttribute("message", "日報提出");
+		return "form/index";
+	}
+
+	 
 }
