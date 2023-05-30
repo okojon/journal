e6d441fb0cd04fc1cdfd97388b907e6a923621f1
kokusai-kato <y-kato@kokusaiexpress.com>
2023-05-30 13:26:20 +0900

    kato fourth commit

--- src/main/java/com/example/demo/dao/JorDao.java
+++ src/main/java/com/example/demo/dao/JorDao.java
@@ -88,4 +88,50 @@ public class JorDao {
 		//Controllerに渡す
 		return resultDb2;
 	}
+
+	//削除(DELETE)
+	public void deleteDb(Long id) {
+		//コンソールに表示
+		System.out.println("削除しました");
+		//DBからデータを削除
+		db.update("delete from journal where id=?", id);
+	}
+
+	//更新画面の表示(SELECT)
+	public List<JorForm> selectOne(Long id) {
+
+		//コンソールに表示
+		System.out.println("編集画面を出します");
+		//データベースから目的の1件を取り出して、そのままresultDB1に入れる
+		List<Map<String, Object>> resultDb1 = db.queryForList("SELECT * FROM journal where id=?", id);
+		//画面に表示しやすい形のList(resultDB2)を用意
+		List<JorForm> resultDb2 = new ArrayList<JorForm>();
+
+		//1件ずつピックアップ
+		for (Map<String, Object> result1 : resultDb1) {
+			//データ1件分を1つのまとまりとするので、EntForm型の「entformdb」を生成
+			JorForm entformdb = new JorForm();
+			//id、nameのデータをentformdbに移す
+			entformdb.setId((int) result1.get("id"));
+			entformdb.setDate((String) result1.get("date"));
+			entformdb.setName((String) result1.get("name"));
+			entformdb.setTitle((String) result1.get("title"));
+			entformdb.setComment((String) result1.get("comment"));
+			//移し替えたデータを持ったentformdbを、resultDB2に入れる
+			resultDb2.add(entformdb);
+		}
+
+		//Controllerに渡す
+		return resultDb2;
+	}
+
+	//更新の実行(UPDATE)
+	public void updateDb(Long id, JorForm entform) {
+		//コンソールに表示
+		System.out.println("編集の実行");
+		//UPDATEを実行
+		db.update("UPDATE journal SET title = ? WHERE id = ?", entform.getTitle(), id);
+
+		db.update("UPDATE journal SET comment = ? WHERE id = ?", entform.getComment(), id);
+	}
 }
